62dc5bb546705b49f2f4f28f68141d7ac2fac317
Animaniac <rdaislabie@gmail.com>
2017-06-07 19:19:23 +0100

    Delete Event button now works

--- EventsApp/Controllers/IndexController.cs
+++ EventsApp/Controllers/IndexController.cs
@@ -12,13 +12,7 @@ namespace EventsApp.Controllers
         public ActionResult Index()
         {
             var entity = new EventsEntity();
-            var myEvents =  entity.MyEvents.Where( x => x.EventDate > DateTime.Now).ToList();
-            //index page wants to retun a countdown list of my current events
-            //countdown only inlcudes name and how long until the event
-            //want to be able to click on the event to open more details on that event
-            //details should include the name, date/time, description
-            //button on the page for adding a new event
-            //each 'event tile' should have an edit/delete icon
+            var myEvents =  entity.MyEvents.Where( x => x.EventDate > DateTime.Now).ToList();            
             return View(myEvents);
         }
         
@@ -37,6 +31,24 @@ namespace EventsApp.Controllers
             var entity = new EventsEntity();
             var eventId = entity.MyEvents.Attach(changedEvent);
             entity.Entry(changedEvent).State = System.Data.Entity.EntityState.Modified;
+            entity.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var entity = new EventsEntity();
+            var eventId = entity.MyEvents.Where(x => x.EventId == id).First();
+
+            return View(eventId);
+        }
+        [HttpPost]
+        public ActionResult Delete(MyEvent eventToDelete)
+        {
+            var entity = new EventsEntity();
+            var currentEvent = entity.MyEvents.Where(x => x.EventId == eventToDelete.EventId).First();
+            entity.MyEvents.Remove(currentEvent);            
             entity.SaveChanges();
             return RedirectToAction("Index");
         }
