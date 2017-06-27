1afc3502b3d501e2b3a7a46c05fe353504197d6a
Animaniac <rdaislabie@gmail.com>
2017-06-27 16:04:18 +0100

    Tested script wont trigger....

--- EventsApp/Scripts/spa.js
+++ EventsApp/Scripts/spa.js
@@ -1,11 +1,11 @@
-﻿$.getJSON("@Url.Action("GetAllEvents", "Event")", function (data) {
+﻿/*$.getJSON("@Url.Action("GetAllEvents", "Event")", function (data) {
     console.log(data);
-});
+}));*/
 
 $('a').on('click', function (e) {
     e.preventDefault();
     let pageRef = $(this).attr('href');
-
+    console.log(pageRef);
     callPage(pageRef);
 });
 
@@ -13,7 +13,7 @@ function callPage(pageRefInput) {
     $.ajax({
         url: pageRefInput,
         type: "get",
-        dataType: text,
+        dataType: "text",
         success: function (response) {
             console.log('the page was loaded', response);
             $('#eventsSection').html(response);
